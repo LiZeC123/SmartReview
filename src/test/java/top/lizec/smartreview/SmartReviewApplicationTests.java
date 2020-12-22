a7eab39fb28d27eeba734f00b65ec0f24184a7d7
lizec <email@example.com>
2020-12-22 22:25:24 +0800

    Initial commit

---
+++ src/test/java/top/lizec/smartreview/SmartReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lizec.smartreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
