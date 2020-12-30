d96f0c6e15e656df083117a7b581938325acd899
lizec <email@example.com>
2020-12-30 16:52:09 +0800

    添加RememberMe相关代码

--- src/main/java/top/lizec/smartreview/config/WebSecurityConfig.java
+++ src/main/java/top/lizec/smartreview/config/WebSecurityConfig.java
@@ -3,9 +3,16 @@ package top.lizec.smartreview.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import top.lizec.smartreview.service.UserServer;
+
+import javax.annotation.Resource;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    UserServer userServer;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -16,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/api/**").hasRole("USER")
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login.html").permitAll()
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .rememberMe().userDetailsService(userServer).rememberMeParameter("remember");
     }
 }
