008a7533888dd18aeccfe85cb87aee3682149b19
LiZeC <tomgreem123@gmail.com>
2021-02-08 12:47:15 +0800

    Swagger UI header部分添加Authorization参数

--- src/main/java/top/lizec/smartreview/SmartReviewApplication.java
+++ src/main/java/top/lizec/smartreview/SmartReviewApplication.java
@@ -9,14 +9,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import io.swagger.annotations.Api;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -34,10 +39,10 @@ public class SmartReviewApplication {
     @Bean
     public Producer captcha() {
         Properties properties = new Properties();
-        properties.setProperty("kaptcha.img.width","150");
-        properties.setProperty("kaptcha.img.height","150");
-        properties.setProperty("kaptcha.textproducer.char.string","ABCDEFGHJKLMNPQRSTUVWXYZabcefeghijkmnpqrstuvwxyz23456789");
-        properties.setProperty("kaptcha.textproducer.char.length","5");
+        properties.setProperty("kaptcha.img.width", "150");
+        properties.setProperty("kaptcha.img.height", "150");
+        properties.setProperty("kaptcha.textproducer.char.string", "ABCDEFGHJKLMNPQRSTUVWXYZabcefeghijkmnpqrstuvwxyz23456789");
+        properties.setProperty("kaptcha.textproducer.char.length", "5");
 
         Config config = new Config(properties);
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
@@ -47,12 +52,21 @@ public class SmartReviewApplication {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        ticketPar.name("Authorization")
+                .description("Authorization For User")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build(); //header中的ticket参数非必填，传空也可以
+        pars.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build().globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
