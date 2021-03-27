606e10cb6dedd5048f960a287ad48301751b1c87
LiZeC <tomgreem123@gmail.com>
2021-03-27 22:01:43 +0800

    添加用户名查询接口

--- src/main/java/top/lizec/smartreview/mapper/UserDao.java
+++ src/main/java/top/lizec/smartreview/mapper/UserDao.java
@@ -1,12 +1,14 @@
 package top.lizec.smartreview.mapper;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-
 import top.lizec.smartreview.entity.User;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 public interface UserDao {
 
     User findByUserEmail(String email);
 
     Long insertUser(User user) throws SQLIntegrityConstraintViolationException;
+
+    User getUserNameById(Integer id);
 }
