c1d4ebdab226cdce6525b7c4ddd01d391d34ad05
LiZeC <tomgreem123@gmail.com>
2021-09-02 20:09:36 +0800

    添加数据库初始化处理

--- service.sh
+++ service.sh
@@ -29,6 +29,15 @@ function update {
   git pull 
 }
 
+function init {
+  dataFile=data/SmartReview.db
+  if [ ! -f "${dataFile}" ]; then
+    echo "Create Init Database."
+    sqlite3 "${dataFile}" < app/sql/init_sqlite.sql
+fi
+}
+
+
 if [ "$1"x == "start"x ]; then
   compileService
   runService
@@ -46,11 +55,14 @@ elif [ "$1"x == "backup"x ]; then
   backup
 elif [ "$1"x == "update"x ]; then
   update
+elif [ "$1"x == "init"x ]; then
+  init
 else
   echo "无效的参数: $1"
   echo ""
   echo "用法: ./service [参数]"
   echo "参数可以选择以下值:"
+  echo "init      初始化数据库"
   echo "start     编译并启动项目"
   echo "stop      停止项目"
   echo "restart   重启项目"
