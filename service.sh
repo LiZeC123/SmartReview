a28b99d8aab613ba0f334e20c3d79256278f74a2
LiZeC <tomgreem123@gmail.com>
2021-09-16 14:35:09 +0800

    添加强制重置数据集选项

--- service.sh
+++ service.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 
-function compileService {
-  docker-compose build
-}
-
-function runService {
-  docker-compose up -d
-}
-
-function stopService {
-  docker-compose down
-}
-
 function backup {
   zip -r SmartReview.zip data/
 }
 
-function update {
-  git pull 
-}
-
 function init {
   dataFile=data/SmartReview.db
-  if [ ! -f "${dataFile}" ||  "$1"x == "-f"x ]; then
+  if [ ! -f "${dataFile}" ]; then
     echo "初始化数据库."
+    sqlite3 "${dataFile}" < app/sql/init_sqlite.sql
+  elif [ "$1"x == "-f"x ]; then
+    echo "重置数据库."
+    rm ${dataFile}
     sqlite3 "${dataFile}" < app/sql/init_sqlite.sql
   else
     echo "数据文件${dataFile}已存在, 使用 -f 参数强制更新."
@@ -31,23 +19,8 @@ function init {
 }
 
 
-if [ "$1"x == "start"x ]; then
-  compileService
-  runService
-elif [ "$1"x == "compile"x ]; then
-  compileService
-elif [ "$1"x == "run"x ]; then
-  runService
-elif [ "$1"x == "stop"x ]; then
-  stopService
-elif [ "$1"x == "restart"x ]; then
-  stopService
-  compileService
-  runService
-elif [ "$1"x == "backup"x ]; then
+if [ "$1"x == "backup"x ]; then
   backup
-elif [ "$1"x == "update"x ]; then
-  update
 elif [ "$1"x == "init"x ]; then
   init $2
 else
@@ -56,11 +29,5 @@ else
   echo "用法: ./service [参数]"
   echo "参数可以选择以下值:"
   echo "init      初始化数据库"
-  echo "start     编译并启动项目"
-  echo "stop      停止项目"
-  echo "restart   重启项目"
-  echo "compile   只编译项目"
-  echo "run       直接运行项目"
-  echo "backup    备份数据文件"
   echo "update    更新项目代码"
 fi
