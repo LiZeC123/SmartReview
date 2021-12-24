820b6215954cb4371f4da800b7060bb5b2bd1b80
LiZeC <tomgreem123@gmail.com>
2021-12-24 14:02:26 +0800

    使用Go实现登录模块

--- app/go.mod
+++ app/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/CodyGuo/godaemon v0.0.0-20200413142854-c36b39fdd071
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- app/main.go
+++ app/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"io"
+	"net/http"
+	"os"
+
 	_ "github.com/CodyGuo/godaemon"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 func main() {
+	// 全局配置需要在开始的时候执行
+	gin.DisableConsoleColor()
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
 	r := gin.Default()
-	r.GET("/api/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.SetTrustedProxies(nil)
+
+	user := r.Group("/api/user")
+	{
+		user.GET("/login", login)
+	}
+
 	r.Run("localhost:8792")
 }
+
+type UserLogin struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func login(c *gin.Context) {
+	var login UserLogin
+	if err := c.ShouldBindJSON(&login); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": true, "token": "<token>"})
+}
+
